11037634f4314813d830ae7b13ec3f716a98a91f
ccukt@yahoo.com <ccukt@yahoo.com@64dd3ded-ea0c-dae4-800a-9adc8ec03cbb>
2013-08-04 10:48:54 +0000

    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s); js.type = 'text/javascript'; fjs.type = 'text/javascript'; js.async=true; fjs.async=true; js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs"); THIS CODE RETURNED BACK

--- trunk/facebook.js
+++ trunk/facebook.js
@@ -1,6 +1,5 @@
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
-
   if (d.getElementById(id)) return;
 var js = document.createElement('script');
 js.type = 'text/javascript';
@@ -10,9 +9,3 @@ js.async = true;
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);
-js.type = 'text/javascript';
-fjs.type = 'text/javascript';
-js.async=true;
-fjs.async=true;
-js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
