040741e97ff3c644cd2e15c2f22b2568a4128dd3
ccukt@yahoo.com <ccukt@yahoo.com@64dd3ded-ea0c-dae4-800a-9adc8ec03cbb>
2013-08-07 15:16:59 +0000

    facebooktwittergoogleplus.js COMBINED LOADING WITH JAVASCRIPT COMBINE CODE 7 8 2013

---
+++ trunk/facebooktwittergoogleplus.js
@@ -0,0 +1,12 @@
+var scripts = ["http://apis.google.com/js/plusone.js","http://connect.facebook.net/en_US/all.js#xfbml=1","http://platform.twitter.com/widgets.js"];
+(function(array) {
+   for (var i = 0, len = array.length; i < len; i++)
+   {
+      var elem = document.createElement('script');
+      elem.type = 'text/javascript';
+      elem.async = true;
+      elem.src = array[i];
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(elem, s);
+   }
+})(scripts);
